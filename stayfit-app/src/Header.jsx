a0fe34a0341395e94e9f77d82e67e30a68045dc9
Nunzia Biele <nunzia.biele@virgilio.it>
2024-09-03 14:31:22 +0200

    creazione landing page

---
+++ stayfit-app/src/Header.jsx
@@ -0,0 +1,21 @@
+import STAYFit from './assets/img/STAYFit(transparent).png'
+
+const Header = () => {
+  return (
+    <header className="bg-teal-700 p-4 flex justify-between items-center">
+    <img src={STAYFit} alt="Stayfit Logo" className="h-20 w-20 mr-3" />
+      {/* <h1 className="text-white text-3xl font-bold text-center mb-4">Stayfit</h1> */}
+      {/* <nav>
+        <ul className="flex space-x-4">
+          <li><a href="#Impostazioni" className="text-white">Impostazioni</a></li>
+          <li><a href="#Home" className="text-white">Home</a></li>
+          <button className="bg-white text-blue-600 px-4 py-2 rounded-full font-semibold">
+          Login
+        </button>
+        </ul>
+      </nav> */}
+    </header>
+  );
+};
+
+export default Header;
---
+++ stayfit-app/src/Parte1.jsx
@@ -0,0 +1,20 @@
+
+
+const Presentazione = () => {
+  return (
+    <section className="bg-teal-700 text-white text-center py-20 w-full">
+      <h2 className="text-5xl font-bold mb-2">IL BENESSERE,
+       PERSONALIZZATO.
+        SEMPLIFICATO</h2>
+      <p className="text-xl mb-4">Unisciti anche tu alla rivoluzione del Fitness,
+      gestisci e monitora i piani di allenamento e nutrizione dei tuoi clienti,
+      offrendo soluzioni personalizzate in modo semplice e veloce.
+      </p>
+      <button className="bg-green-500 text-black font-semibold px-10 py-3">
+        Unisciti ora!
+      </button>
+    </section>
+  );
+};
+
+export default Presentazione;
