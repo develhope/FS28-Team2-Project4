30c46099baccd4e0c3a9752402234b61eed3a438
Maurilio Valenti <maurilio.valenti@gmail.com>
2024-09-09 18:23:44 +0200

    added Textbox in FormCliente

--- stayfit-app/src/App.jsx
+++ stayfit-app/src/App.jsx
@@ -1,23 +1,13 @@
 import './App.css';
-import Button from './MyComponents/Button';
-import MobileButton from './MyComponents/MobileButton';
+import Form from './FormCliente'
 import Textbox from './MyComponents/Textbox';
-import { CardMenu } from './MyComponents/CardMenu';
-import { SubCards } from './MyComponents/subCards';
-import { CardProvider } from './MyComponents/CardProvider';
 
 const App = () => {
   return (
     <div className="App">
-      <div className="flex flex-col gap-5">
-        <Textbox id={'name'} type={'text'} label={'Nome'} />
-        <MobileButton />
-        <Button text={'Unisciti ora!'} />
-        <CardProvider>
-          <CardMenu></CardMenu>
-          <SubCards></SubCards>
-        </CardProvider>
-      </div>
+      <Form>
+        <Textbox></Textbox>
+      </Form>
     </div>
   );
 };
--- stayfit-app/src/FormCliente.jsx
+++ stayfit-app/src/FormCliente.jsx
@@ -1,27 +1,28 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback } from 'react';
+import Textbox from './MyComponents/Textbox.jsx';
 
 const Form = () => {
   const [step, setStep] = useState(0);
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    birthDate: "",
-    gender: "",
-    email: "",
-    phone: "",
-    weight: "",
-    height: "",
-    allergies: "",
-    foodIntolerances: "",
-    activityLevel: "",
-    fitnessGoals: "",
-    workoutPreferences: "",
-    availableTime: "",
+    firstName: '',
+    lastName: '',
+    birthDate: '',
+    gender: '',
+    email: '',
+    phone: '',
+    weight: '',
+    height: '',
+    allergies: '',
+    foodIntolerances: '',
+    activityLevel: '',
+    fitnessGoals: '',
+    workoutPreferences: '',
+    availableTime: '',
     photo: null,
-    username: "",
-    password: "",
-    confirmPassword: "",
+    username: '',
+    password: '',
+    confirmPassword: '',
   });
 
   const [showPassword, setShowPassword] = useState(false);
@@ -30,7 +31,7 @@ const Form = () => {
     const { name, value, type, checked } = e.target;
     setFormData((prev) => ({
       ...prev,
-      [name]: type === "checkbox" ? checked : value,
+      [name]: type === 'checkbox' ? checked : value,
     }));
   };
 
@@ -90,7 +91,7 @@ const Form = () => {
       setStep(step + 1);
     } else {
       alert(
-        "Compila tutti i campi obbligatori o assicurati che le password corrispondano."
+        'Compila tutti i campi obbligatori o assicurati che le password corrispondano.'
       );
     }
   };
@@ -99,55 +100,60 @@ const Form = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
+
     if (isSubmitted) {
-      alert("Modulo già inviato.");
+      alert('Modulo già inviato.');
       return;
     }
 
     if (isStepValid(step)) {
-      setIsSubmitted(true); 
-      alert("Modulo inviato con successo!");
+      setIsSubmitted(true);
+      alert('Modulo inviato con successo!');
     } else {
-      alert("La Password non corrisponde.");
+      alert('La Password non corrisponde.');
     }
   };
 
   const steps = [
     {
-      label: "Nome e Cognome",
+      label: 'Nome e Cognome',
       fields: (
         <>
-          <label>Nome</label>
-          <input
+          <Textbox
+            label="Nome"
             type="text"
+            id="firstName"
             name="firstName"
             value={formData.firstName}
             onChange={handleChange}
             required
           />
-          <label>Cognome</label>
-          <input
+          <Textbox
+            label="Cognome"
             type="text"
+            id="lastName"
             name="lastName"
             value={formData.lastName}
             onChange={handleChange}
             required
           />
-          <label>Data di Nascita</label>
-          <input
+          <Textbox
+            label="Data di Nascita"
             type="date"
             name="birthDate"
             value={formData.birthDate}
             onChange={handleChange}
             required
           />
-          <label>Sesso</label>
+          <label  className={'absolute left-3 top-2 transition-all duration-200 ease-in-out bg-primary-blue px-0.5 bg-left bg-no-repeat'}>Sesso</label>
           <select
             name="gender"
             value={formData.gender}
             onChange={handleChange}
             required
+            className="peer border-2 w-[300px] h-10 bg-transparent border-secondary-gray cursor-pointer
+  text-[#C5C5C5] pl-[12px] pr-[12px] rounded-[6px] outline-none transition-all
+  duration-300 focus:ring-secondary-green hover:border-secondary-green focus:border-secondary-green"
           >
             <option value="">Seleziona</option>
             <option value="male">Maschio</option>
@@ -157,19 +163,22 @@ const Form = () => {
       ),
     },
     {
-      label: "Contatti",
+      label: 'Contatti',
       fields: (
         <>
-          <label>Email</label>
-          <input
+          <Textbox
+            label="Email"
             type="email"
+            id="email"
             name="email"
             value={formData.email}
             onChange={handleChange}
+            required
           />
-          <label>Numero di Telefono</label>
-          <input
+          <Textbox
+            label="Numero di Telefono"
             type="tel"
+            id="phone"
             name="phone"
             value={formData.phone}
             onChange={handleChange}
@@ -178,20 +187,22 @@ const Form = () => {
       ),
     },
     {
-      label: "Misure",
+      label: 'Misure',
       fields: (
         <>
-          <label>Peso (kg)</label>
-          <input
+          <Textbox
+            label="Peso (kg)"
             type="number"
+            id="weight"
             name="weight"
             value={formData.weight}
             onChange={handleChange}
             required
           />
-          <label>Altezza (cm)</label>
-          <input
+          <Textbox
+            label="Altezza (cm)"
             type="number"
+            id="height"
             name="height"
             value={formData.height}
             onChange={handleChange}
@@ -201,19 +212,21 @@ const Form = () => {
       ),
     },
     {
-      label: "Allergie e Intolleranze",
+      label: 'Allergie e Intolleranze',
       fields: (
         <>
-          <label>Allergie</label>
-          <input
+          <Textbox
+            label="Allergie"
             type="text"
+            id="allergies"
             name="allergies"
             value={formData.allergies}
             onChange={handleChange}
           />
-          <label>Intolleranze Alimentari</label>
-          <input
+          <Textbox
+            label="Intolleranze Alimentari"
             type="text"
+            id="foodIntolerances"
             name="foodIntolerances"
             value={formData.foodIntolerances}
             onChange={handleChange}
@@ -222,7 +235,7 @@ const Form = () => {
       ),
     },
     {
-      label: "Fitness",
+      label: 'Fitness',
       fields: (
         <>
           <label>Livello di Attività Fisica</label>
@@ -231,19 +244,26 @@ const Form = () => {
             value={formData.activityLevel}
             onChange={handleChange}
             required
+            className="peer border-2 w-[300px] h-10 bg-transparent border-secondary-gray cursor-pointer
+  text-[#C5C5C5] pl-[12px] pr-[12px] rounded-[6px] outline-none transition-all
+  duration-300 focus:ring-secondary-green hover:border-secondary-green focus:border-secondary-green"
           >
             <option value="">Seleziona</option>
             <option value="sedentary">Sedentario</option>
             <option value="moderatelyActive">Moderatamente Attivo</option>
             <option value="active">Attivo</option>
-            <option value="veryActive"> Molto Attivo</option>
+            <option value="veryActive">Molto Attivo</option>
           </select>
+
           <label>Obiettivi di Fitness</label>
           <select
             name="fitnessGoals"
             value={formData.fitnessGoals}
             onChange={handleChange}
             required
+            className="peer border-2 w-[300px] h-10 bg-transparent border-secondary-gray cursor-pointer
+  text-[#C5C5C5] pl-[12px] pr-[12px] rounded-[6px] outline-none transition-all
+  duration-300 focus:ring-secondary-green hover:border-secondary-green focus:border-secondary-green"
           >
             <option value="">Seleziona</option>
             <option value="weightLoss">Perdita di peso</option>
@@ -257,22 +277,28 @@ const Form = () => {
             </option>
             <option value="rehabilitation">Riabilitazione</option>
           </select>
+
           <label>Preferenze di Allenamento</label>
           <select
             name="workoutPreferences"
             value={formData.workoutPreferences}
             onChange={handleChange}
             required
+            className="peer border-2 w-[300px] h-10 bg-transparent border-secondary-gray cursor-pointer
+  text-[#C5C5C5] pl-[12px] pr-[12px] rounded-[6px] outline-none transition-all
+  duration-300 focus:ring-secondary-green hover:border-secondary-green focus:border-secondary-green"
           >
             <option value="">Seleziona</option>
             <option value="cardio">Cardio</option>
             <option value="flexibility">Flessibilità</option>
-            <option value="outdoorActivities">Attività all&#39;aperto</option>
+            <option value="outdoorActivities">Attività all&apos;aperto</option>
             <option value="gym">Palestra</option>
           </select>
-          <label>Pratichi già uno sport?</label>
-          <input
+
+          <Textbox
+            label="Pratichi già uno sport?"
             type="text"
+            id="availableTime"
             name="availableTime"
             value={formData.availableTime}
             onChange={handleChange}
@@ -282,7 +308,7 @@ const Form = () => {
       ),
     },
     {
-      label: "Foto",
+      label: 'Foto',
       fields: (
         <>
           <label>Carica una Foto</label>
@@ -298,28 +324,31 @@ const Form = () => {
       ),
     },
     {
-      label: "Account",
+      label: 'Account',
       fields: (
         <div className="client-form">
-          <label>Username</label>
-          <input
+          <Textbox
+            label="Username"
             type="text"
+            id="username"
             name="username"
             value={formData.username}
             onChange={handleChange}
             required
           />
-          <label>Password</label>
-          <input
-            type={showPassword ? "text" : "password"}
+          <Textbox
+            label="Password"
+            type={showPassword ? 'text' : 'password'}
+            id="password"
             name="password"
             value={formData.password}
             onChange={handleChange}
             required
           />
-          <label>Conferma Password</label>
-          <input
-            type={showPassword ? "text" : "password"}
+          <Textbox
+            label="Conferma Password"
+            type={showPassword ? 'text' : 'password'}
+            id="confirmPassword"
             name="confirmPassword"
             value={formData.confirmPassword}
             onChange={handleChange}
