caa531f02bac145c274aba0d1c48b3f346e54dbc
mario-sica/Mario <mario97sica@gmail.com>
2024-09-12 20:58:54 +0200

    Test: Select fix and testing

--- stayfit-app/src/App.jsx
+++ stayfit-app/src/App.jsx
@@ -4,8 +4,9 @@ import { ToggleSwitch } from './MyComponents/ToggleSwitch';
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="App flex flex-col justify-center items-center gap-5">
       <TrySelect></TrySelect>
+      <h1 className='text-white'>Sono un...</h1>
       <ToggleSwitch></ToggleSwitch>
     </div>
   );
--- stayfit-app/src/MyComponents/SelectBox.jsx
+++ stayfit-app/src/MyComponents/SelectBox.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 export const SelectBox = ({
   label,
@@ -8,46 +8,36 @@ export const SelectBox = ({
   options,
   required,
 }) => {
-  const [inputVal, setInputVal] = useState(value);
   const [isFocused, setIsFocused] = useState(false);
 
-  useEffect(() => {
-    setInputVal(value);
-  }, [value]);
-
-  const handleFocus = () => {
-    setIsFocused(true);
-  };
+  const handleFocus = () => setIsFocused(true);
 
   const handleBlur = () => {
-    if (inputVal === '') {
+    if (!value) {
       setIsFocused(false);
     }
   };
 
   return (
-    <div className='relative w-[300px] h-10'>
+    <div className="relative w-[300px] h-10">
       <label
         htmlFor={name}
         className={`absolute opacity-0 left-4 top-2 transition-all duration-200 ease-in-out bg-primary-blue px-0.5 bg-left bg-no-repeat ${
-            isFocused || inputVal
-              ? 'opacity-100 top-[-8px] left-[0.75rem] text-xs text-secondary-green glow-text bg-[length:100%_100%]'
-              : 'top-2 text-base text-secondary-gray cursor-text bg-[length:0%_100%]'
-          }`}
+          isFocused || value
+            ? 'opacity-100 top-[-8px] left-[0.75rem] text-xs text-secondary-green glow-text bg-[length:100%_100%]'
+            : 'top-2 text-base text-secondary-gray cursor-text bg-[length:0%_100%]'
+        }`}
       >
         {label}
       </label>
       <select
-        className="peer border-2 w-[300px] h-10 bg-transparent border-secondary-gray cursor-text
+        className="custom-select peer border-2 w-[300px] h-10 bg-transparent border-secondary-gray cursor-text
           caret-[#C5C5C5] text-[#C5C5C5] pl-[12px] pr-[12px] rounded-[6px] outline-none transition-all
           duration-300 focus:ring-secondary-green hover:border-secondary-green focus:border-secondary-green
           glow-effect focus:transition-all focus:duration-300"
         name={name}
-        value={inputVal}
-        onChange={(e) => {
-          setInputVal(e.target.value);
-          onChange(e);
-        }}
+        value={value}
+        onChange={onChange}
         onFocus={handleFocus}
         onBlur={handleBlur}
         {...(required && { required })}
