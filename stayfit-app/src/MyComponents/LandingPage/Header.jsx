9ca12cd7303b8db98c451ff0096e4ac593179177
Nunzia Biele <nunzia.biele@virgilio.it>
2024-09-17 21:34:55 +0200

    ultimi fix landing page

--- stayfit-app/src/MyComponents/LandingPage/Header.jsx
+++ stayfit-app/src/MyComponents/LandingPage/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className= " p-4">
     <img src={STAYFit} alt="Stayfit Logo" className="absolute top-5 md:top-12 right-5 md:right-10 w-20 md:w-40 h-auto" />
-    <h2 className="block md:hidden border-b-2 border-secondary-green text-4xl text-white font-bold mb- py-8 text-center">Stay<span className="text-custom-green">Fit</span></h2>
+    <h2 className="block md:hidden border-b-2 border-secondary-green text-4xl text-white font-bold mb- py-8 text-center">Stay<span className="text-secondary-green">Fit</span></h2>
     </header>
   );
 };
