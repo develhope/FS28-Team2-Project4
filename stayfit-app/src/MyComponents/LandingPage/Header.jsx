e55f2305420d0c379e46b2e27c87906ab2dddbb1
Nunzia Biele <nunzia.biele@virgilio.it>
2024-09-17 18:41:08 +0200

    aggiunto il login alla landing page

--- stayfit-app/src/MyComponents/LandingPage/Header.jsx
+++ stayfit-app/src/MyComponents/LandingPage/Header.jsx
@@ -1,20 +1,11 @@
 import STAYFit from '../../assets/img/STAYFit(transparent).png'
 
+
 const Header = () => {
   return (
     <header className= " p-4">
     <img src={STAYFit} alt="Stayfit Logo" className="absolute top-5 md:top-12 right-5 md:right-10 w-20 md:w-40 h-auto" />
     <h2 className="block md:hidden border-b-2 border-secondary-green text-4xl text-white font-bold mb- py-8 text-center">Stay<span className="text-custom-green">Fit</span></h2>
-      {/* <h1 className="text-white text-3xl font-bold text-center mb-4">Stayfit</h1> */}
-      {/* <nav>
-        <ul className="flex space-x-4">
-          <li><a href="#Impostazioni" className="text-white">Impostazioni</a></li>
-          <li><a href="#Home" className="text-white">Home</a></li>
-          <button className="bg-white text-blue-600 px-4 py-2 rounded-full font-semibold">
-          Login
-        </button>
-        </ul>
-      </nav> */}
     </header>
   );
 };
--- stayfit-app/src/MyComponents/LandingPage/Presentazione.jsx
+++ stayfit-app/src/MyComponents/LandingPage/Presentazione.jsx
@@ -6,31 +6,73 @@ const Presentazione = () => {
   const [isRegisterOpen, setRegisterOpen] = useState(false);
   const [isLoginOpen, setLoginOpen] = useState(false);
 
+  const handleLoginClick = () => {
+    setLoginOpen(true);
+  };
+
+  const handleRegisterClick = () => {
+    setRegisterOpen(true);
+    setLoginOpen(false);
+  };
+
+  const handleOutsideClick = (e) => {
+    if (e.target.id === 'overlay') {
+      setRegisterOpen(false);
+    }
+  };
+
   return (
     <section className="text-white text-center md:text-left p-12 py-20 w-full">
       <h2 className="text-5xl font-bold mb-4">
         IL BENESSERE,
-        <br></br>
+        <br />
         PERSONALIZZATO.
-        <br></br>
+        <br />
         SEMPLIFICATO
       </h2>
       <p className="text-xl text-white mb-6">
         Unisciti anche tu alla rivoluzione del Fitness, gestisci e monitora i
         piani di allenamento
-        <br></br>e nutrizione dei tuoi clienti, offrendo soluzioni
-        personalizzate in modo semplice e veloce.
+        <br />e nutrizione dei tuoi clienti, offrendo soluzioni personalizzate
+        in modo semplice e veloce.
       </p>
       <Button
         type="button"
-        onClick={() => setRegisterOpen(true)}
+        onClick={handleRegisterClick}
         text={'Unisciti ora!'}
       />
 
       {isRegisterOpen && (
-        <div className='h-full w-full flex justify-center items-center z-10'>
-          <div className="flex justify-center items-center z-10 p-10 w-[400px] h-[350px] fixed bg-primary-blue border-2 border-secondary-green rounded-lg shadow-2xl">
-            <FormProfessionista onClose={() => setRegisterOpen(false)} />
+        <div
+          id="overlay"
+          onClick={handleOutsideClick}
+          className="fixed inset-0 bg-black bg-opacity-20 backdrop-blur-sm flex justify-center items-center z-10"
+        >
+          <div className="relative z-20 p-10 w-[400px] h-auto bg-primary-blue border-2 border-secondary-green rounded-lg shadow-2xl">
+            <button
+              onClick={() => setRegisterOpen(false)}
+              className="absolute top-2 right-2 text-white"
+            >
+              X
+            </button>
+
+            {!isLoginOpen ? (
+              <FormProfessionista onClose={() => setRegisterOpen(false)} />
+            ) : (
+              <FormProfessionista onClose={() => setRegisterOpen(false)} />
+            )}
+
+            {!isLoginOpen && (
+              <p className="text-white text-center mt-6">
+                Sei già iscritto?{' '}
+                <span
+                  onClick={handleLoginClick}
+                  className="text-secondary-green underline cursor-pointer"
+                >
+                  Login
+                </span>
+              </p>
+            )}
           </div>
         </div>
       )}
