af43d12e384bc91088f457cdd0f88d9025a6c562
mario-sica/Mario <mario97sica@gmail.com>
2024-09-18 21:17:01 +0200

    Fix: Responsiveness CardMenu - Card

--- stayfit-app/src/App.jsx
+++ stayfit-app/src/App.jsx
@@ -1,18 +1,12 @@
 import './App.css';
-import { TrySelect } from './MyComponents/TrySelect';
-import { ToggleSwitch } from './MyComponents/ToggleSwitch';
 import { CardMenu } from './MyComponents/CardMenu';
-import { SubCards } from './MyComponents/SubCards';
 import { CardProvider } from './MyComponents/CardProvider';
-import LoginForm from './MyComponents/LoginForm';
 
 const App = () => {
   return (
     <div className="App flex flex-col justify-center items-center gap-5">
-      <LoginForm />
       <CardProvider>
         <CardMenu></CardMenu>
-        <SubCards></SubCards>
       </CardProvider>
     </div>
   );
