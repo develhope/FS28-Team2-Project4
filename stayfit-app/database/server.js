aab9908e097eb76a6a65191aa8553824115ef6aa
Luigi Cavalli <luigi_cavalli@outlook.com>
2024-10-03 21:22:13 +0200

    minor fix

--- stayfit-app/database/server.js
+++ stayfit-app/database/server.js
@@ -1,12 +1,14 @@
 import express from 'express';
+import morgan from 'morgan';
+import 'express-async-errors';
 
 const app = express();
 const port = 3000;
 
-app.use(express.json());
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-  res.send('Hello!');
+  res.status(200).json({ msg: 'Hello!' });
 });
 
 app.listen(port, () => {
