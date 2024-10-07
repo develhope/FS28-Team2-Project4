b13871c3d1b47c62ce606e7f72a36eb453e32467
Nunzia Biele <nunzia.biele@virgilio.it>
2024-10-07 17:31:11 +0200

    pull

--- stayfit-app/src/App.jsx
+++ stayfit-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Alimentazione from './MyComponents/Alimentazione';
 import './App.css';
-import { CardCliente } from './MyComponents/LandingPage/CardCliente';
+import { CardCliente } from './MyComponents/CardCliente';
 
 const App = () => {
   return (
