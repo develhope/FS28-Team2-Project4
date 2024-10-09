4b4f26ac40690a96894c68bfbac0519c6ff75750
mario-sica/Mario <mario97sica@gmail.com>
2024-10-10 00:23:59 +0200

    Added: db & db comunication Mod: several changes

--- stayfit-app/database/database.js
+++ stayfit-app/database/database.js
@@ -6,11 +6,11 @@ dotenv.config();
 const postgreConnect = pgPromise();
 
 const settings = {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+    host: 'localhost',
+    port: 5432,
+    database: 'stayfit_database',
+    user: 'postgres',
+    password: 'Admin'
 };
 
 export const database = postgreConnect(settings);
--- stayfit-app/database/server.js
+++ stayfit-app/database/server.js
@@ -4,6 +4,7 @@ import Joi from 'joi';
 import 'express-async-errors';
 import dotenv from 'dotenv';
 import { database } from './database.js';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -12,62 +13,63 @@ const port = 3000;
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors({origin: 'http://localhost:5173'}))
 
-const fileSchema = (allowedMimeTypes) => {
-  return Joi.object({
-    mimetype: Joi.string()
-      .valid(...allowedMimeTypes)
-      .required(),
-    data: Joi.any().required(),
-  });
-};
+// const fileSchema = (allowedMimeTypes) => {
+//   return Joi.object({
+//     mimetype: Joi.string()
+//       .valid(...allowedMimeTypes)
+//       .required(),
+//     data: Joi.any().required(),
+//   });
+// };
 
 const schema = Joi.object({
-  professionType: Joi.string()
+  profession_type: Joi.string()
     .valid('personalTrainer', 'nutrizionista', 'entrambi')
     .required(),
-  birthDate: Joi.date().iso().required(),
-  certifications: Joi.array()
-    .items(fileSchema(['pdf']))
-    .required(),
-  confirmPassword: Joi.string().min(6).required().valid(Joi.ref('password')),
+  birth_date: Joi.date().iso().required(),
+  // certifications: Joi.array()
+  //   .items(fileSchema(['pdf']))
+  //   .required(),
+  // confirmPassword: Joi.string().min(6).required().valid(Joi.ref('password')),
   description: Joi.string().min(5).allow(''),
   email: Joi.string().email().required(),
   experience: Joi.number().integer().min(0).required(),
-  firstName: Joi.string().required(),
-  lastName: Joi.string().required(),
+  first_name: Joi.string().required(),
+  last_name: Joi.string().required(),
   password: Joi.string().min(6).required(),
   phone: Joi.string().pattern(/^\d+$/).min(10).max(15).required(),
-  privacyPolicyAccepted: Joi.boolean().valid(true).required(),
-  profilePhoto: fileSchema(['image/jpeg', 'image/png']).required(),
-  receiveUpdates: Joi.boolean().required(),
+  privacy_policy_accepted: Joi.boolean().valid(true).required(),
+  // profilePhoto: fileSchema(['image/jpeg', 'image/png']).required(),
+  receive_updates: Joi.boolean().required(),
   referral: Joi.string().allow(''),
-  socialAccountName: Joi.string().allow(''),
-  socialNetwork: Joi.string()
+  social_account_name: Joi.string().allow(''),
+  social_network: Joi.string()
     .valid('facebook', 'instagram', 'twitter', 'linkedin')
     .allow(''),
-  subscriptionType: Joi.string().valid('Free', 'Starter', 'Premium').required(),
-  taxCode: Joi.string()
+  subscription_type: Joi.string().valid('Free', 'Starter', 'Premium').required(),
+  tax_code: Joi.string()
     .pattern(/^\d{11}$/)
     .allow(''),
-  termsAccepted: Joi.boolean().valid(true).required(),
+  terms_accepted: Joi.boolean().valid(true).required(),
   username: Joi.string().required(),
-  workArea: Joi.string().required(),
+  work_area: Joi.string().required(),
 });
 
-app.get('/users', async (req, res) => {
+app.get('/professionals', async (req, res) => {
   try {
-    const users = await database.any('SELECT * FROM users');
-    res.status(200).json(users);
+    const professionals = await database.any('SELECT * FROM professionals');
+    res.status(200).json(professionals);
   } catch (error) {
     res.status(500).json({ error: 'Errore nel recuperare gli utenti' });
   }
 });
 
-app.get('/users/:id', async (req, res) => {
+app.get('/professionals/:id', async (req, res) => {
   const { id } = req.params;
   try {
-    const user = await database.oneOrNone('SELECT * FROM users WHERE id = $1', [
+    const user = await database.oneOrNone('SELECT * FROM professionals WHERE id = $1', [
       id,
     ]);
     if (!user) {
@@ -79,37 +81,38 @@ app.get('/users/:id', async (req, res) => {
   }
 });
 
-app.post('/users', async (req, res) => {
+app.post('/professionals', async (req, res) => {
   const { error, value } = schema.validate(req.body);
   if (error) {
     return res.status(400).json({ error: error.details[0].message });
   }
   try {
     const newUser = await database.one(
-      'INSERT INTO users (professionType, birthDate, certifications, confirmPassword, description, email, experience, firstName, lastName, password, phone, privacyPolicyAccepted, profilePhoto, receiveUpdates, referral, socialAccountName, socialNetwork, subscriptionType, taxCode, termsAccepted, username, workArea) VALUES (${professionType}, ${birthDate}, ${certifications}, ${confirmPassword}, ${description}, ${email}, ${experience}, ${firstName}, ${lastName}, ${password}, ${phone}, ${privacyPolicyAccepted}, ${profilePhoto}, ${receiveUpdates}, ${referral}, ${socialAccountName}, ${socialNetwork}, ${subscriptionType}, ${taxCode}, ${termsAccepted}, ${username}, ${workArea}) RETURNING id',
+      'INSERT INTO professionals (profession_type, birth_date, description, email, experience, first_name, last_name, password, phone, privacy_policy_accepted, receive_updates, referral, social_account_name, social_network, subscription_type, tax_code, terms_accepted, username, work_area) VALUES (${profession_type}, ${birth_date}, ${description}, ${email}, ${experience}, ${first_name}, ${last_name}, ${password}, ${phone}, ${privacy_policy_accepted}, ${receive_updates}, ${referral}, ${social_account_name}, ${social_network}, ${subscription_type}, ${tax_code}, ${terms_accepted}, ${username}, ${work_area}) RETURNING id',
       value
     );
     res.status(201).json({ msg: 'User created successfully', id: newUser.id });
   } catch (error) {
+    console.error('Errore nella creazione dell\'utente:', error);
     res.status(500).json({ error: "Errore nella creazione dell'utente" });
   }
 });
 
-app.put('/users/:id', async (req, res) => {
+app.put('/professionals/:id', async (req, res) => {
   const { error, value } = schema.validate(req.body);
   const { id } = req.params;
   if (error) {
     return res.status(400).json({ error: error.details[0].message });
   }
   try {
-    const user = await database.oneOrNone('SELECT * FROM users WHERE id = $1', [
+    const user = await database.oneOrNone('SELECT * FROM professionals WHERE id = $1', [
       id,
     ]);
     if (!user) {
       return res.status(404).json({ msg: 'User not found' });
     }
     await database.none(
-      'UPDATE users SET professionType = ${professionType}, birthDate = ${birthDate}, certifications = ${certifications}, confirmPassword = ${confirmPassword}, description = ${description}, email = ${email}, experience = ${experience}, firstName = ${firstName}, lastName = ${lastName}, password = ${password}, phone = ${phone}, privacyPolicyAccepted = ${privacyPolicyAccepted}, profilePhoto = ${profilePhoto}, receiveUpdates = ${receiveUpdates}, referral = ${referral}, socialAccountName = ${socialAccountName}, socialNetwork = ${socialNetwork}, subscriptionType = ${subscriptionType}, taxCode = ${taxCode}, termsAccepted = ${termsAccepted}, username = ${username}, workArea = ${workArea} WHERE id = ${id}',
+      'UPDATE professionals SET profession_type = ${profession_type}, birth_date = ${birth_date}, description = ${description}, email = ${email}, experience = ${experience}, first_name = ${first_name}, last_name = ${last_name}, password = ${password}, phone = ${phone}, privacy_policy_accepted = ${privacy_policy_accepted}, receive_updates = ${receive_updates}, referral = ${referral}, social_account_name = ${social_account_name}, social_network = ${social_network}, subscription_type = ${subscription_type}, tax_code = ${tax_code}, terms_accepted = ${terms_accepted}, username = ${username}, work_area = ${work_area} WHERE id = ${id}',
       value
     );
     res.status(200).json({ msg: 'User updated successfully' });
@@ -118,19 +121,54 @@ app.put('/users/:id', async (req, res) => {
   }
 });
 
-app.delete('/users/:id', async (req, res) => {
+app.delete('/professionals/:id', async (req, res) => {
   const { id } = req.params;
   try {
-    const user = await database.oneOrNone('SELECT * FROM users WHERE id = $1', [
+    const user = await database.oneOrNone('SELECT * FROM professionals WHERE id = $1', [
       id,
     ]);
     if (!user) {
       return res.status(404).json({ msg: 'User not found' });
     }
-    await database.none('DELETE FROM users WHERE id = $1', [id]);
+    await database.none('DELETE FROM professionals WHERE id = $1', [id]);
     res.status(200).json({ msg: 'User deleted successfully' });
   } catch (error) {
     res.status(500).json({ error: "Errore nell'eliminare l'utente" });
+  }
+});
+
+app.get('/foods/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const alimento = await database.oneOrNone('SELECT * FROM foods WHERE id = $1', [id]);
+    if (!alimento) {
+      return res.status(404).json({ error: 'Alimento non trovato' });
+    }
+    res.status(200).json(alimento);
+  } catch (error) {
+    console.error('Errore nel recuperare l\'alimento:', error);
+    res.status(500).json({ error: 'Errore nel recuperare l\'alimento' });
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await database.oneOrNone(
+      'SELECT * FROM professionals WHERE email = $1 AND password = $2',
+      [email, password]
+    );
+
+    if (user) {
+      res.status(200).json({ msg: 'Login effettuato con successo', userId: user.id });
+    } else {
+      res.status(401).json({ error: 'Email o password non corretti' });
+    }
+  } catch (error) {
+    console.error('Errore durante il login:', error);
+    res.status(500).json({ error: 'Si è verificato un errore durante il login' });
   }
 });
 
