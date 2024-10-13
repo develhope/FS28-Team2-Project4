22249cdfaa08d5ec5aecc2c517d97da057ab4b52
mario-sica/Mario <mario97sica@gmail.com>
2024-10-13 17:11:29 +0200

    Fix: Dashboard name visualization

--- stayfit-app/src/MyComponents/Iscrizione.jsx
+++ stayfit-app/src/MyComponents/Iscrizione.jsx
@@ -1,4 +1,38 @@
+import { useState, useEffect } from "react";
+
 export const Iscrizione = () => {
+
+  const [userInfo, setUserInfo] = useState(null);
+
+useEffect(() => {
+  const fetchUserInfo = async () => {
+    const userId = localStorage.getItem('userId');
+
+    if (userId) {
+      try {
+        const res = await fetch(`http://localhost:3000/professionals/${userId}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (res.ok) {
+          const data = await res.json();
+          setUserInfo(data);
+        } else {
+          setErrorMessage('Impossibile recuperare le informazioni dell\'utente');
+        }
+      } catch (error) {
+        console.error('Errore durante il recupero delle informazioni dell\'utente:', error);
+        setErrorMessage('Si è verificato un errore durante il recupero delle informazioni');
+      }
+    }
+  };
+
+  fetchUserInfo();
+}, []);
+
     return (
     <div className="bg-activated-card text-white text-left p-6 rounded-lg shadow-md dropdown">
       <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
@@ -7,8 +41,8 @@ export const Iscrizione = () => {
         <div className="bg-activated-card p-6 rounded-lg">
           <h4 className="font-bold mb-4 text-secondary-green">Informazioni di fatturazione:</h4>
           <p className='py-2'>Metodo di pagamento:</p>
-          <p className='py-2'>Mastercard 5555 4444 3333 2222</p>
-          <p>Scadenza: 08/24</p>
+          <p className='py-2'>Mastercard **** **** **** 2222</p>
+          <p>Scadenza: 12/24</p>
           <a href="#" className="text-secondary-green mt-2 block p-2 underline" aria-label="Aggiungi un metodo di pagamento">
             + Aggiungi un metodo di pagamento
           </a>
@@ -25,11 +59,11 @@ export const Iscrizione = () => {
         {/* Dettagli dell'abbonamento */}
         <div className="bg-activated-card p-6 rounded-lg">
           <h4 className="font-bold mb-4 text-secondary-green">I dettagli del tuo abbonamento:</h4>
-          <p className='py-3'>Hai sottoscritto il nostro piano: Annuale</p>
-          <p>Nome: Mario</p>
-          <p>Cognome: Rossi</p>
-          <p className='py-3'>Professione: Nutrizionista</p>
-          <p>Scadenza: 12/24</p>
+          <p className='py-3'>Hai sottoscritto il nostro piano: {userInfo ? userInfo.subscription_type : 'Caricamento...'}</p>
+          <p>Nome: {userInfo ? userInfo.first_name : 'Caricamento...'}</p>
+          <p>Cognome: {userInfo ? userInfo.last_name : 'Caricamento...'}</p>
+          <p className='py-3'>Professione: {userInfo ? userInfo.profession_type : 'Caricamento...'}</p>
+          <p>Scadenza: 12/2025</p>
           <a href="#" className="text-secondary-green mt-2 block py-3 underline" aria-label="Rinnova in anticipo">
             Rinnova in anticipo
           </a>
